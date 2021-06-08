7b94a7e5b0f7c6438f7431dc3fd9d9e9c7eb6b25
Alexander <alexander.davidson87@gmail.com>
2021-06-08 10:50:12 -0500

    Start pokemonList
    Started pokemonList variable array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-alert('Hello World');
-
-let favFood = "Pepperoni Pizza";
-document.write(favFood);
+let pokemonList = [
+  {name: 'bulbuasaur', height: .7, type: ['grass', 'poison']}
+  {name: 'charmander', height: .6, type: 'fire'}
+  {name: 'squirtle', height: .5, type: 'water'}
+  {name: 'caterpie', height: .3, type: 'bug'}
+  {name: 'weedle', height: .3, type: ['bug', 'poison']}
+  {name: 'pidgey', height: .3, type: ['flying', 'normal']}
+  {name: 'rattata', height: .3, type: 'normal'}
+  {name: 'spearow', height: .3, type: ['flying', 'normal']}
+  {name: 'ekans', height: 2, type: 'poison'}
+  {name: 'pikachu', height: .4, type: 'electric'}
+]
