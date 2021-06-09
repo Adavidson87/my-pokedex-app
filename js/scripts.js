e7ab45c75ba7ede2d89bf1d3e59a0a77bcfc6ed1
Alexander <alexander.davidson87@gmail.com>
2021-06-08 23:16:53 -0500

    Update scripts.js
    Added commas to the end of each object in the pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 let pokemonList = [
-  {name: 'bulbuasaur', height: .7, type: ['grass', 'poison']}
-  {name: 'charmander', height: .6, type: 'fire'}
-  {name: 'squirtle', height: .5, type: 'water'}
-  {name: 'caterpie', height: .3, type: 'bug'}
-  {name: 'weedle', height: .3, type: ['bug', 'poison']}
-  {name: 'pidgey', height: .3, type: ['flying', 'normal']}
-  {name: 'rattata', height: .3, type: 'normal'}
-  {name: 'spearow', height: .3, type: ['flying', 'normal']}
-  {name: 'ekans', height: 2, type: 'poison'}
+  {name: 'bulbuasaur', height: .7, type: ['grass', 'poison']},
+  {name: 'charmander', height: .6, type: 'fire'},
+  {name: 'squirtle', height: .5, type: 'water'},
+  {name: 'caterpie', height: .3, type: 'bug'},
+  {name: 'weedle', height: .3, type: ['bug', 'poison']},
+  {name: 'pidgey', height: .3, type: ['flying', 'normal']},
+  {name: 'rattata', height: .3, type: 'normal'},
+  {name: 'spearow', height: .3, type: ['flying', 'normal']},
+  {name: 'ekans', height: 2, type: 'poison'},
   {name: 'pikachu', height: .4, type: 'electric'}
 ]
