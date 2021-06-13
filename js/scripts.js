7b124055e30e8c098ed21388548aa4a8925e8fb7
Alexander <alexander.davidson87@gmail.com>
2021-06-13 12:54:33 -0500

    started using pokemon api
    Removed static array and added pokepon api, added loadDetails and loadList functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,20 @@
 //Wrapping pokemonList in IIFE
 let pokemonRepository = (function () {
   //Array of pokemon
-  let pokemonList = [
-    {name: 'Bulbuasaur',height: .7,type: ['grass', 'poison']},
-    {name: 'Ivysaur',height: 1,type: ['grass', 'poison']},
-    {name: 'Veusaur', height: 2, type: ['grass', 'poison']},
-    {name: 'Charmander', height: .6, type: 'fire'},
-    {name: 'Charmeleon', height: 1.1, type: 'fire'},
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Squirtle', height: .5, type: 'water'},
-    {name: 'Wrtortle', height: 1, type: 'water'},
-    {name: 'Blastoise', height: 1.6, type: 'water'},
-    {name: 'Caterpie', height: .3, type: 'bug'},
-    {name: 'Metapod', height: .7, type: 'bug'},
-    {name: 'Butterfree', height: 1.1, type: ['bug', 'flying']},
-    {name: 'Weedle', height: .3, type: ['bug', 'poison']},
-    {name: 'Kakuna', height: .6, type: ['bug', 'poison']},
-    {name: 'Beedrill', height: 1, type: ['bug', 'poison']},
-    {name: 'Pidgey', height: .3, type: ['flying', 'normal']},
-    {name: 'Pidgeotto', height: 1.1, type: ['flying', 'normal']},
-    {name: 'Pidgeot', height: 1.5, type: ['flying', 'normal']},
-    {name: 'Rattata', height: .3, type: 'normal'},
-    {name: 'Raticate', height: .7, type: 'normal'},
-    {name: 'Spearow', height: .3, type: ['flying', 'normal']},
-    {name: 'Fearow', height: 1.2, type: ['flying', 'normal']},
-    {name: 'Ekans', height: 2, type: 'poison'},
-    {name: 'Arbok', height: 3.5, type: 'poison'},
-    {name: 'Pikachu', height: .4, type: 'electric'},
-    {name: 'Raichu', height: .8, type: 'electric'}
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   //Adds pokemon to the list
   function add(pokemon){
-    if (typeof(pokemon) === 'object'){
-      pokemonList.push(pokemon);
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "detailsUrl" in pokemon
+      )
+    pokemonList.push(pokemon);
+    else {
+      return document.write("Not correct information");
     }
-    else
-    return document.write("Not correct information");
   }
 
   //prints pokemon list
@@ -58,8 +36,42 @@ let pokemonRepository = (function () {
     pokemonList.appendChild(listpokemon);
   }
 
+  //shows details when button is pushed
   function showDetails(pokemon){
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+  //gets pokemon details from api
+  function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(details) {
+      //adds details to the add addListItem
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function(e) {
+      console.error(e);
+    });
   }
 
   //list of usuable functions
@@ -67,10 +79,15 @@ let pokemonRepository = (function () {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
-    showDetails: showDetails
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  //Now the data is loaded
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
